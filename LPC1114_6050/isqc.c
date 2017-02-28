cb11c4daac575ff63c9b212dfa472dae0599733c
Tanishq Dubey <tanishq.dubey@gmail.com>
2017-02-28 12:41:07 -0600

    I2C lib done?

--- LPC1114_6050/isqc.c
+++ LPC1114_6050/isqc.c
@@ -1,5 +1,6 @@
-#include <stdint.h>
 #include <i2c.h>
+
+I2CTransaction *curr_transaction;
 
 void I2CInit() {
     unsigned div;
@@ -17,6 +18,37 @@ void I2CInit() {
 	I2C0SCLL = (div & 0xffff);
     // Enable I2C interrupts (Pg 507)
     ISER = 1;
-    // Enable I2C altogether
+    // Enable I2C altogether (Pg 243)
     I2C0CONSET = BIT6;
+}
+
+void I2CStop() {
+    // Stop signal (Pg 243)
+    I2C0CONSET = BIT4;
+}
+
+void I2CStart() {
+    // Stop signal (Pg 243)
+    I2C0CONSET = BIT5;
+}
+
+int  I2CTransact(I2CTransaction *transact) {
+	unsigned t_out=0xfffff;
+    // Why do this? It makes sure that only one I2C transaction happens at a time
+    //  because we are using a global pointer for it. Probably bad practice but I
+    //  am OK with it for right now
+	curr_transaction = transact;
+	curr_transaction->idx=0;
+	curr_transaction->status=0;
+    I2CStart();
+
+	while ((curr_transaction->status ==  0) && (t_out--)){
+        // Wait for transaction to complete.
+    }
+
+	if (curr_transaction->status) {
+		return -1;  // something went wrong maybe.
+    } else {
+		return 0; 	//  Not clear about status values and success - come back here.
+    }
 }
---
+++ LPC1114_6050/isqc.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include "lcp111x.h"
+
+#ifndef I2C_H
+#define I2C_H
+
+#define MAX_I2C_DATA 64
+
+typedef struct {
+    int mode;
+    uint8_t s_addr;
+    unsigned idx;
+    unsigned cnt;
+    int status;
+    uint8_t data[MAX_I2C_DATA];
+} I2CTransaction;
+
+void I2CInit();
+void I2CStop();
+void I2CStart();
+int  I2CTransact(I2CTransaction *transact);
+
+#endif /* I2C_H */
